fa5c5c44139111fddcef9bbb65c68a646b0b030f
kerunix <pierre@kissmy.co>
2022-07-08 16:06:11 +0200

    feat: sort on table

--- components/Base/Table/BaseTableHeaderItem.vue
+++ components/Base/Table/BaseTableHeaderItem.vue
@@ -1,20 +1,58 @@
 <script lang="ts" setup>
 interface Props {
   sortable?: boolean
+  sortKey?: string
+  justify?: 'left' | 'center' | 'right'
 }
 
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<Props>(), {
   sortable: true,
+  sortKey: '',
+  justify: 'left',
+})
+
+const { registerHeaderItem, activeItem, setActiveItem, queryState } = injectStrict(TABLE_STATE_KEY)
+
+const isActive = computed(() => activeItem.value === props.sortKey)
+
+registerHeaderItem(props.sortKey)
+
+function onClick() {
+  setActiveItem(props.sortKey)
+}
+
+const alignmentClasses = computed(() => {
+  switch (props.justify) {
+    case 'center':
+      return 'text-center'
+    case 'right':
+      return 'text-right'
+    default:
+      return 'text-left'
+  }
+})
+
+onMounted(() => {
+  if (process.env.NODE_ENV === 'development') {
+    if (props.sortable && props.sortKey.length === 0) {
+      console.warn('[HR TABLE ITEM] => You required this item to be sortable but didnt provide a sortKey.')
+    }
+  }
 })
 </script>
 
 <template>
-  <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 dark:text-gray-50 sm:pl-6">
-    <a href="#" class="group inline-flex">
-      ID
-      <span v-if="sortable" class="invisible ml-2 flex-none rounded text-gray-700 group-hover:visible group-focus:visible">
-        <ChevronDownIconOutline class="h-5 w-5" aria-hidden="true" />
+  <th scope="col" class="px-4 py-4 text-sm font-semibold text-gray-900 dark:text-gray-50 md:px-6 md:py-5" :class="alignmentClasses">
+    <button class="group inline-flex" @click="onClick">
+      <slot />
+      <span
+        v-if="sortable"
+        class="ml-2 flex-none rounded"
+        :class="[isActive ? 'bg-gray-300 text-gray-700' : 'text-gray-400']"
+      >
+        <ChevronDownIconOutline v-if="queryState.orderDir === 'desc'" class="h-5 w-5" aria-hidden="true" />
+        <ChevronUpIconOutline v-else class="h-5 w-5" aria-hidden="true" />
       </span>
-    </a>
+    </button>
   </th>
 </template>
