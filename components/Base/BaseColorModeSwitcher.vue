2261515e553cd55db99ff535634339f0436fc950
kerunix <pierre@kissmy.co>
2022-07-10 14:43:51 +0200

    feat: trying vue3 popper

--- components/Base/BaseColorModeSwitcher.vue
+++ components/Base/BaseColorModeSwitcher.vue
@@ -3,8 +3,13 @@ const { isDark, toggleColorMode } = useDarkMode()
 </script>
 
 <template>
-  <button @click="toggleColorMode">
-    <SunIconOutline v-if="isDark" class="h-6 w-6 text-gray-800 hover:text-turquoise-900 dark:text-gray-300 dark:hover:text-turquoise-400" />
-    <MoonIconOutline v-else class="h-6 w-6 text-gray-800 hover:text-turquoise-900 dark:text-gray-300 dark:hover:text-turquoise-400" />
-  </button>
+  <BaseTooltip>
+    <button @click="toggleColorMode">
+      <SunIconOutline v-if="isDark" class="h-6 w-6 text-gray-800 hover:text-turquoise-900 dark:text-gray-300 dark:hover:text-turquoise-400" />
+      <MoonIconOutline v-else class="h-6 w-6 text-gray-800 hover:text-turquoise-900 dark:text-gray-300 dark:hover:text-turquoise-400" />
+    </button>
+    <template #content>
+      {{ isDark ? 'Light Mode' : 'Dark Mode' }}
+    </template>
+  </BaseTooltip>
 </template>
---
+++ components/Base/BaseTooltip.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Popper from 'vue3-popper'
+</script>
+
+<template>
+  <Popper v-bind="$attrs" arrow placement="auto" hover open-delay="100" close-delay="100">
+    <slot />
+    <template #content="props">
+      <slot name="content" v-bind="props" />
+    </template>
+  </Popper>
+</template>
+
+<style lang="postcss" scoped>
+  :deep(.popper) {
+    @apply py-1 px-2 bg-turquoise-900 text-white text-sm rounded shadow-md hover:bg-turquoise-900;
+  }
+
+  :deep(.popper #arrow::before) {
+    @apply bg-turquoise-900
+  }
+
+  :deep(.popper:hover),
+  :deep(.popper:hover > #arrow::before) {
+    @apply bg-turquoise-900;
+  }
+</style>
